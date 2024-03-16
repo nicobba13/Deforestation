2d743162648520451dff186206a022a8a1251e8d
nicobba13 <155686676+nicobba13@users.noreply.github.com>
2024-03-16 11:54:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+import pathlib
 import random
 
 import streamlit as st
+import torch
 
 from PIL import Image
+from fastai.vision.all import *
 
+if platform.system() == 'Windows':
+    # fastai Load_Learner uses PosixPath, use a trick to load the model
+    _saved_hack = pathlib.PosixPath
+    pathlib.PosixPath = pathlib.WindowsPath
+    classification_model = load_learner('models/palmoil_fastai_resnet18.pth')
+    pathlib.PosixPath = _saved_hack
+else:
+    classification_model = load_learner('models/palmoil_fastai_resnet18.pth')
 
 def classify_image(image) -> bool:
     """Classify if an image has palm trees or not
@@ -14,7 +25,10 @@ def classify_image(image) -> bool:
     Returns:
     - bool: True if image contains palm trees
     """
-    return random.choice([True, False])
+    tensor = torch.tensor(np.asarray(Image.open(image).convert('RGB').resize((256, 256))))
+    results = classification_model.predict(tensor)
+    return results[0] == '1'
+    # return random.choice([True, False])
     
 def analyze_image(image):
     """Analyze parts of image that most contributed to classification
@@ -27,7 +41,7 @@ def analyze_image(image):
     """
     return Image.open(image).convert("L")
     
-st.write("# Palm Tree Detector 🎄✨")
+st.write("# Palm Tree Detector🌴")
 
 st.write("Oh yeah baby 😎")
 
@@ -42,12 +56,15 @@ if image_file is not None:
     st.image(bytes_image, use_column_width=True)
 
 if st.button("Detect palm trees", disabled=(image_file is None)):
-    result = classify_image(bytes_image)
+    result = classify_image(image_file)
     if result:
         st.write('Palm trees detected')
     else:
         st.write('Palm trees not detected')
         
+    with st.expander('Sensitivity Analysis', expanded=False):
+        analysis_image = analyze_image(image_file)
+        st.image(analysis_image, width=300)
     with st.expander('Sensitivity Analysis', expanded=False):
         analysis_image = analyze_image(image_file)
         st.image(analysis_image, width=300)
